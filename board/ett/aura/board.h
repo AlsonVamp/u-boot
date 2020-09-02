7707b3a7b987c63949475df6937505d10bf63a0c
Aleksandr Nazarov <alsnazarov@gmail.com>
2020-09-02 23:58:21 +0300

    Add ett aura board

---
+++ board/ett/aura/board.h
@@ -0,0 +1,21 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * board.h
+ *
+ * TI AM335x boards information header
+ *
+ * Copyright (C) 2011, Texas Instruments, Incorporated - http://www.ti.com/
+ */
+
+#ifndef _BOARD_H_
+#define _BOARD_H_
+
+void enable_uart0_pin_mux(void);
+// void enable_uart1_pin_mux(void);
+// void enable_uart2_pin_mux(void);
+// void enable_uart3_pin_mux(void);
+// void enable_uart4_pin_mux(void);
+// void enable_uart5_pin_mux(void);
+void enable_board_pin_mux(void);
+
+#endif
